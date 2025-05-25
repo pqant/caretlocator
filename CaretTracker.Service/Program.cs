5c9c998fe866314ce764a594ba8b15f0d1628ccb
Eralp Erat <eralperat@microsoft.com>
2025-05-25 09:47:40 +0200

    TASK-000: Update code files

---
+++ CaretTracker.Service/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CaretTracker.Service
+{
+    internal class Program
+    {
+        // Windows API imports will be added here
+        // TODO: Add GetGUIThreadInfo and ClientToScreen imports
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Caret Tracker Service Starting...");
+            
+            try
+            {
+                // Main service loop will be implemented here
+                await RunServiceAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+
+        private static async Task RunServiceAsync()
+        {
+            // Service implementation will be added here
+            // This is just a placeholder for now
+            while (true)
+            {
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
